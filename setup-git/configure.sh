0e02c60b934c2ba253632b2b7efe2d223d09671b
Tobias Sommer <tobias.sommer.dev@mailbox.org>
2023-07-20 11:50:56 +0200

    git setup script

---
+++ setup-git/configure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo Configuring in $SHELL
+
+git config --global user.name "Tobias Sommer"
+git config --global user.email "tobias.sommer.dev@mailbox.org"
+git config --global color.ui auto
