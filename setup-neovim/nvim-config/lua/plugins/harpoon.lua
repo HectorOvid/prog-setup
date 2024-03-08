65c0ac0b9f9559b5bdf970d66aa7eaf0cbf4b187
Tobias Sommer <tobias.sommer.dev@mailbox.org>
2024-03-08 01:03:53 +0100

    fix harpoon

--- setup-neovim/nvim-config/lua/plugins/harpoon.lua
+++ setup-neovim/nvim-config/lua/plugins/harpoon.lua
@@ -2,9 +2,31 @@ return {
 	{
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
-		-- config = function()
-		-- 	require("harpoon"):setup()
-		-- end,
+		config = function()
+			local harpoon = require("harpoon")
+
+			harpoon:setup()
+
+			vim.keymap.set("n", "<leader>a", function()
+				harpoon:list():append()
+			end)
+			vim.keymap.set("n", "<C-e>", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
+
+			vim.keymap.set("n", "<leader><C-j>", function()
+				harpoon:list():select(1)
+			end)
+			vim.keymap.set("n", "<leader><C-k>", function()
+				harpoon:list():select(2)
+			end)
+			vim.keymap.set("n", "<leader><C-l>", function()
+				harpoon:list():select(3)
+			end)
+			vim.keymap.set("n", "<leader><C-ö>", function()
+				harpoon:list():select(4)
+			end)
+		end,
 		opts = {
 			menu = {
 				width = vim.api.nvim_win_get_width(0) - 4,
